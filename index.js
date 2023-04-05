06f09e7b24bf5a842bcfa4c00cd50b5312c591af
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-04-05 16:39:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
 // Code your solutions in this file
+function writeCards(array,event) {
+    const thankYouNotes = [];
+    for (let i = 0; i < array.length; i++){
+       const message = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+       thankYouNotes.push(message);
+    } 
+    return thankYouNotes;
+}
+function countDown(number) {
+    let num = number;
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
